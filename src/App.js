07ebcfee482a71b3eefb2c995deef7475856279c
CrIsTiLaz <cristilazea18@gmail.com>
2023-07-14 14:25:41 +0300

    Qualification component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
+import Qualification2 from "./components/qualification/Qualification2";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Qualification2 />
       </main>
     </>
   );
